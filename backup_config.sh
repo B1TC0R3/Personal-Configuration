7fb9a4313f37eb5a93831122602620efd6849f21
Ginthom <thomas@gingele.de>
2022-06-08 14:11:15 +0200

    Automated backup at: Mi 8. Jun 14:11:15 CEST 2022

--- backup_config.sh
+++ backup_config.sh
@@ -11,6 +11,16 @@ files=(
 /usr/share/awesome/themes/b1tc0r3/theme.lua
 )
 
+echo "Copying files..."
 for elem in ${files[@]}; do
-	install -DC $elem $1/$elem
+	install -DC $elem $1/$elem 1>/dev/null && echo -e "Backed up(\e[36m`date`\e[39m): $elem"
 done
+echo -e "Copying finished (\e[36m`date`\e[39m)!"
+
+echo "Commiting changes..."
+git add $1
+git commit -m "Automated backup at: `date`"
+echo -e "Changes commited (\e[36m`date`\e[39m)"
+
+echo "Uploading files to git..."
+git push &>/dev/null && echo "Files uploaded!"
