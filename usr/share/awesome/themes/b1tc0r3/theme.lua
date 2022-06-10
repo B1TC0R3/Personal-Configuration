1b4aaccd34d61cd42b5557789e79f0c8c6daf8b2
B1TC0R3 <thomas@gingele.de>
2022-06-10 15:01:21 +0200

    Automated backup at: Fr 10. Jun 15:01:21 CEST 2022

--- usr/share/awesome/themes/b1tc0r3/theme.lua
+++ usr/share/awesome/themes/b1tc0r3/theme.lua
@@ -97,7 +97,11 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path.."b1tc0r3/wallpaper_left.jpg" 
+theme.wallpapers = {	
+	themes_path.."b1tc0r3/wallpaper_left.jpg",
+	themes_path.."b1tc0r3/wallpaper_right.jpg"
+}
+theme.wallpaper = function( s ) return theme.wallpapers[s.index] end
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
